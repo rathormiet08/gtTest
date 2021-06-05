b0f4c1bbf6484063dce318ed40fb8500c09d2411
Sudheer Rathore <rathormiet08@gmail.com>
2021-06-05 14:47:58 +0530

    "change into maven"

---
+++ src/main/java/com/git/test/GitMain.java
@@ -0,0 +1,27 @@
+package com.git.test;
+
+import java.util.Scanner;
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(System.in);
+			String hungry = sc.nextLine();
+			if ("Yes" == hungry || "y" == hungry || "Y" == hungry) {
+				System.out.println("eat samosa");
+			} else {
+				System.out.println("do your work");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
